3c861034579907f451030df310dd744427147e83
Zakhar <zakhar-garbuzov@inbox.ru>
2017-07-19 22:29:53 +0300

    добавлена процедура отображения накопленной информации сенсоров

--- serv.py
+++ serv.py
@@ -9,18 +9,18 @@ serv_sock.listen(10)
 sens = 0
 
 while True:
-    # Бесконечно обрабатываем входящие подключения
     client_sock, client_addr = serv_sock.accept()
     print('Connected by', client_addr)
     
     while True:
-        # Пока клиент не отключился, читаем передаваемые 
-        # им данные и отправляем их обратно
         data = client_sock.recv(1024)
-        print("DATA: ", data)
+        if data != '':
+            sens = sens + int(data)
+
+        #print("DATA: ", data)
+        print("FULL SENSOR: ", sens)
         if not data: 
-            # Клиент отключился
             break
-        client_sock.sendall(data)
+        client_sock.sendall(str(sens))
 
     client_sock.close()
