3763e43fbf27ae0479a00d0d6af0645c89f709e8
Zakhar <zakhar-garbuzov@inbox.ru>
2017-07-19 21:40:59 +0300

    First commit Сервер отображает кто к нему подключился и отображает принятые данные. Клиент отправляет данные и принимает ответ

---
+++ client.py
@@ -0,0 +1,9 @@
+# Клиент
+import socket
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('192.168.1.200', 53210))
+client_sock.sendall(b'Hello, world')
+data = client_sock.recv(1024)
+client_sock.close()
+print('Received', repr(data))
